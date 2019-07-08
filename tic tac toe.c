7b0ce1fc552744dde8d93d43b19c4229bec420ba
NabilaAnwar <44870942+NabilaAnwar@users.noreply.github.com>
2019-07-09 00:19:48 +0600

    first commit

---
+++ tic tac toe.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,n;
+
+    for(i = 0 ; i <= 2; i++)
+    {
+        for(j = 1; j<=1; j++)
+        {
+            printf("\n----|----|----");
+        }
+        printf("\n");
+    }
+    return 0;
+}
